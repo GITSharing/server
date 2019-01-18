f32fbcafbf2745dd430aeb6311caf9900f0b8896
ChristianHS23 <christian.sihotang23@gmail.com>
2019-01-18 11:06:21 +0700

    get Top3

--- routes/gifs.js
+++ routes/gifs.js
@@ -6,6 +6,7 @@ const { GifController } = require('../controllers');
 router.get('/', GifController.getAll);
 router.post('/', upload.multer.single('gif'), upload.sendUploadToGCS, GifController.create);
 router.delete('/:id', GifController.delete);
+router.get('/top3', GifController.getTop3)
 
 
 module.exports = router;
